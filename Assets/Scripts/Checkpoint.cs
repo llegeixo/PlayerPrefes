0a5c5305d452390ffd691efa51ba54dc2b940647
llegeixo <gabriel.tallon.lopez@gmail.com>
2024-01-22 20:02:44 +0100

    finished commit

--- Assets/Scripts/Checkpoint.cs
+++ Assets/Scripts/Checkpoint.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class Checkpoint : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private Vector3 _playerPosition;
     void Start()
     {
-        
+        LoadData();
     }
 
-    // Update is called once per frame
-    void Update()
+    
+    public void OnTriggerEnter(Collider other)
     {
+        if(other.CompareTag("Checkpoint"))
+        {
+            Debug.Log("Player entered checkpoint");
+            PlayerPrefs.SetFloat("positionX", transform.position.x);
+            PlayerPrefs.SetFloat("positionZ", transform.position.z);
+            PlayerPrefs.SetFloat("positionY", transform.position.y);
+
+        }
+    
+    }
+
+    public void LoadData()
+    {
+        Debug.Log("Loading player data");
+        float x = PlayerPrefs.GetFloat("positionX", 0f);
+        float y = PlayerPrefs.GetFloat("positionY", 1f);
+        float z = PlayerPrefs.GetFloat("positionZ", 0f);
+        _playerPosition = new Vector3(x, y, z);
+
         
+        transform.position = _playerPosition;
     }
 }
